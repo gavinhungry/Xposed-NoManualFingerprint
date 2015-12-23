5310f1d4a1470d13017435a3fc8fcdddb6bffd63
Gavin Lloyd <gavinhungry@gmail.com>
2015-12-22 16:33:12 -0800

    Init commit

---
+++ README.md
@@ -0,0 +1,18 @@
+Xposed-NoManualFingerprint
+==========================
+Xposed module to disable fingerprint unlock when manually locked.
+
+Build
+-----
+
+    $ ant build
+
+Install
+-------
+
+    $ adb install dist/NoManualFingerprint.apk
+
+License
+-------
+Released under the terms of the
+[MIT license](http://tldrlegal.com/license/mit-license). See **LICENSE**.
---
+++ src/io/gavinhungry/xposed/nomanualfingerprint/NoManualFingerprint.java
@@ -0,0 +1,29 @@
+package io.gavinhungry.xposed.nomanualfingerprint;
+
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+import de.robv.android.xposed.XC_MethodReplacement;
+
+public class NoManualFingerprint implements IXposedHookLoadPackage {
+
+  @Override
+  public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
+    if (!lpparam.packageName.equals("com.android.systemui")) {
+      return;
+    }
+
+     XposedHelpers.findAndHookMethod("com.android.internal.widget.LockPatternUtils$StrongAuthTracker", lpparam.classLoader,
+      "isFingerprintAllowedForUser", int.class, new XC_MethodReplacement() {
+
+      @Override
+      protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+        int userId = (Integer) param.args[0];
+        int strongAuthForUser = (Integer) XposedHelpers.callMethod(param.thisObject, "getStrongAuthForUser", userId);
+
+        int STRONG_AUTH_NOT_REQUIRED = XposedHelpers.getIntField(param.thisObject, "STRONG_AUTH_NOT_REQUIRED");
+        return (strongAuthForUser & ~STRONG_AUTH_NOT_REQUIRED) == 0;
+      }
+    });
+  }
+}
